8e8fe8ab33a4a81203602a2b2ef1cc7efaaf2e53
Margaret Berry <codeberry1@gmail.com>
2018-04-23 19:48:34 -0700

    add delete button

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -22,7 +22,9 @@ class IdeasContainer extends Component {
                 titleRef= {input => this.title = input}
                 resetNotification={this.resetNotification} />)
             } else {
-              return(<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />)
+              return(<Idea idea={idea} key={idea.id}
+                onClick={this.enableEditing}
+                onDelete={this.deleteIdea} />)
             }
           })}
         </div>
@@ -87,6 +89,16 @@ class IdeasContainer extends Component {
   enableEditing = (id) => {
     this.setState({editingIdeaId: id},
     () => { this.title.focus() })
+  }
+
+  deleteIdea = (id) => {
+    axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+    .then(response => {
+      const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+      const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+      this.setState({ideas: ideas})
+    })
+    .catch(error => console.log(error))
   }
 }
 
